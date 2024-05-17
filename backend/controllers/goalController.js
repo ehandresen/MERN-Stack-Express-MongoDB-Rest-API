85f76437746534662007213340369c2f44fe3532
ehandresen <eirik.horgen.andresen@gmail.com>
2024-05-17 14:53:01 +0200

    connected to mongodb

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,3 +1,5 @@
+import asyncHandler from 'express-async-handler';
+
 const goals = [
   {
     id: 1,
@@ -16,42 +18,42 @@ const goals = [
 // @desc get all goals
 // @route GET /api/goals
 // @access Private
-const getGoals = async (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json(goals);
-};
+});
 
 // @desc get single goal
 // @route GET /api/goals:id
 // @access Private
-const getGoal = async (req, res) => {
+const getGoal = asyncHandler(async (req, res) => {
   console.log(req.params);
   const id = parseInt(req.params.id);
   res.status(200).json(goals.find((goal) => goal.id === id));
-};
+});
 
 // @desc create a goal
 // @route POST /api/goals
 // @access Private
-const createGoal = async (req, res) => {
+const createGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field');
   }
   res.status(200).json(goals);
-};
+});
 
 // @desc update a goal
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = async (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json(goals);
-};
+});
 
 // @desc delete a goal
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json(goals);
-};
+});
 
 export { getGoals, getGoal, createGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import colors from 'colors';
 import router from './routes/goalRoutes.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
